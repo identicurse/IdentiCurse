69379e14722cd834b7e799353377ed4d3643c8f8
Luke Slater <tinmachin3@gmail.com>
2010-11-02 22:13:36 +0000

    another possible fix?

--- identicurse.py
+++ identicurse.py
@@ -119,7 +119,7 @@ class IdentiCurse(object):
             try:
                 self.notice_window.addstr(y,4, text.encode("utf-8"))
             except curses.error:
-                self.notice_window.addstr(y,4, "Caution: Terminal too shit to display this notice.".encode())
+                self.notice_window.addstr(y,4, str("Caution: Terminal too shit to display this notice."))
             
             y += 2
  
