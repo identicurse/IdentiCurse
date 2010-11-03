34b947aafdaa4d37b89b6247267499c635400cae
Luke Slater <tinmachin3@gmail.com>
2010-11-03 21:07:10 +0000

    made /kill and /burn aliases for nuke

--- identicurse.py
+++ identicurse.py
@@ -213,7 +213,7 @@ class IdentiCurse(object):
 
                     self.tabs.append(Profile(self.conn, self.notice_window,user))
                     self.current_tab = len(self.tabs) - 1
-                elif tokens[0] == "/spamreport" or tokens[0] == "/sr" or tokens[0] == "/nuke":
+                elif tokens[0] == "/spamreport" or tokens[0] == "/sr" or tokens[0] == "/nuke" or tokens[0] == "/kill" or tokens[0] == "/burn":
                     id = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]['user']['id']
                     username = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]["user"]["screen_name"]
                     status = "@support !sr @%s UID %d %s" % (username, id, " ".join(tokens[2:]))
