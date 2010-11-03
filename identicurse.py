6ab1daee37904184af7388a8bcd39fbfc45dc65c
Luke Slater <tinmachin3@gmail.com>
2010-11-03 19:36:06 +0000

    New tabbing system, rudimentary initial support for profiles, will expand shortly (try tab 5)

--- identicurse.py
+++ identicurse.py
@@ -13,24 +13,84 @@ import locale
 locale.setlocale(locale.LC_ALL, '')
 code = locale.getpreferredencoding()
 
+# In The Future this can just inherit from a Tab object 
+# In fact eventually I'll just implement the tabs as Tab too
+class Profile(object):
+    def __init__(self, conn, window, id):
+        self.conn = conn
+        self.window = window
+        self.id = id
+
+        self.update()
+
+    def update(self):
+        # Note ID can accept screen name or ID in the API. Smart API. Good API.
+        self.profile = self.conn.users_show(screen_name=self.id)
+
+    def display(self):
+        self.window.erase()
+        title = self.profile['screen_name'].capitalize().encode("utf-8") + "'s Profile"
+        self.window.addstr(0, 4, title)
+
+class Timeline(object):
+    def __init__(self, conn, window, timeline):
+        self.conn = conn
+        self.window = window
+        self.timeline_type = timeline
+
+        self.html_regex = re.compile("<(.|\n)*?>")  # compile this here and store it so we don't need to compile the regex every time 'source' is used
+
+    def update(self):
+        if self.timeline_type == "home":
+            self.timeline = self.conn.statuses_home_timeline(count=25, page=0)
+        elif self.timeline_type == "mentions":
+            self.timeline  = self.conn.statuses_mentions(count=25, page=0)
+        elif self.timeline_type == "direct":
+            self.timeline = self.conn.direct_messages(count=25, page=0)
+        elif self.timeline_type == "public":
+            self.timeline = self.conn.statuses_public_timeline()
+
+    def display(self):
+        self.window.erase()
+
+        y = 0
+        c = 1
+
+        maxc = self.window.getmaxyx()[0] / 3
+        maxx = self.window.getmaxyx()[1]
+
+        for n in self.timeline:
+            if self.timeline_type == "direct":
+                user = unicode(n["sender"]["screen_name"])
+                source_msg = "" # source parameter cannot be retrieved from a direct, wtf?
+            else:
+                user = unicode(n["user"]["screen_name"])
+                raw_source_msg = "from %s" % (n["source"])
+                source_msg = self.html_regex.sub("", raw_source_msg)    # strip out the link tag that identi.ca adds to some clients' source info
+            
+            self.window.addstr(y, 0, str(c))
+            self.window.addstr(y, 3, user)
+            self.window.addstr(y, maxx - (len(source_msg) + 2), source_msg)  # right margin of 2 to match the left indentation
+            y += 1
+            text = n["text"]
+
+            try:
+                self.window.addstr(y, 4, text.encode("utf-8"))
+            except curses.error:
+                self.window.addstr(y, 4, str("Caution: Terminal too shit to display this notice."))
+            
+            y += 2
+            c += 1
+
+            if c == maxc:
+                break
+
 class IdentiCurse(object):
     """Contains Main IdentiCurse application"""
     
     def __init__(self):
-
         self.config = json.loads(open('config.json').read())
         self.conn = StatusNet(self.config['api_path'], self.config['username'], self.config['password'])
-        
-        self.html_regex = re.compile("<(.|\n)*?>")  # compile this here and store it so we don't need to compile the regex every time 'source' is used
-
-        self.timelines = { 
-            "home": [],
-            "mentions": [],
-            "direct": [],
-            "public": [] 
-        }
-        self.current_timeline = "home"
-        
         curses.wrapper(self.initialise)
 
     def initialise(self, screen):
@@ -40,21 +100,36 @@ class IdentiCurse(object):
         y, x = screen.getmaxyx()
         self.main_window = screen.subwin(y-2, x-3, 2, 2)
         self.main_window.box(0, 0)
+        self.main_window.scrollok(1)
+        self.main_window.idlok(1)
 
         y, x = self.main_window.getmaxyx()
         self.entry_window = self.main_window.subwin(1, x-10, 4, 5)
         self.text_entry = textpad.Textbox(self.entry_window)
 
         self.notice_window = self.main_window.subwin(y-6, x-4, 7, 5)
+        
+        self.tabs = [
+            Timeline(self.conn, self.notice_window, "home"),
+            Timeline(self.conn, self.notice_window, "mentions"),
+            Timeline(self.conn, self.notice_window, "direct"),
+            Timeline(self.conn, self.notice_window, "public")
+        ]
+        self.current_tab = 0
 
-        #self.main_window.scrollok(1)
-        #self.main_window.idlok(1)
-        #self.main_window.erase()
+        self.update_tabs()
+        self.display_current_tab()
 
-        self.update_timelines()
-        self.display_current_timeline()
+        self.tabs.append(Profile(self.conn, self.notice_window, 'reality'))
 
         self.loop()
+
+    def update_tabs(self):
+        for tab in self.tabs:
+            tab.update()
+
+    def display_current_tab(self):
+        self.tabs[self.current_tab].display()
 
     def loop(self):
         running = True
@@ -63,30 +138,22 @@ class IdentiCurse(object):
             input = self.main_window.getch()
 
             if input == ord("r"):
-                self.update_timelines()
-                self.update_current()
-            elif input == ord("1"):
-                self.current_timeline = "home"
-            elif input == ord("2"):
-                self.current_timeline = "mentions"
-            elif input == ord("3"):
-                self.current_timeline = "direct"
-            elif input == ord("4"):
-                self.current_timeline = "public"
+                self.update_tabs()
             elif input == ord("i"):
                 self.parse_input(self.text_entry.edit(self.validate))
             elif input == ord("q"):
                 running = False
+            
+            for x in range(0, len(self.tabs)):
+                if input == ord(str(x+1)):
+                    self.current_tab = x
 
-            self.display_current_timeline()
+            self.display_current_tab()
             self.main_window.refresh()
 
         self.quit();
 
     def validate(self, ch):
-        #curses.endwin()
-        #print ch
-
         if ch == 127:
             self.text_entry.do_command(263)
         else:
@@ -98,15 +165,15 @@ class IdentiCurse(object):
                 tokens = input.split(" ")
                 
                 if tokens[0] == "/reply" or tokens[0] == "/r":
-                    id = self.timelines[self.current_timeline][int(tokens[1]) - 1]['id']
-                    username = self.timelines[self.current_timeline][int(tokens[1]) - 1]["user"]["screen_name"]
+                    id = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]['id']
+                    username = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]["user"]["screen_name"]
                     status = "@" + username + " " + " ".join(tokens[2:])
                     self.conn.statuses_update(status, "IdentiCurse", int(id))
                 elif tokens[0] == "/fav" or tokens[0] == "/f":
-                    id = self.timelines[self.current_timeline][int(tokens[1]) - 1]['id']
+                    id = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]['id']
                     self.conn.favorites_create(id)
                 elif tokens[0] == "/repeat" or tokens[0] == "/rt":
-                    id = self.timelines[self.current_timeline][int(tokens[1]) - 1]['id']
+                    id = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]['id']
                     self.conn.statuses_retweet(id)
                 elif tokens[0] == "/direct" or tokens[0] == "/dm" or tokens[0] == "/d":
                     screen_name = tokens[1]
@@ -115,7 +182,7 @@ class IdentiCurse(object):
                     id = self.conn.users_show(screen_name=screen_name)['id']
                     self.conn.direct_messages_new(screen_name, id, " ".join(tokens[2:]), source="IdentiCurse")
                 elif tokens[0] == "/delete" or tokens[0] == "/del":
-                    id = self.timelines[self.current_timeline][int(tokens[1]) - 1]['id']
+                    id = self.tabs[self.current_tab].timeline[int(tokens[1]) - 1]['id']
                     self.conn.statuses_destroy(id)
             else:
                 self.conn.statuses_update(input, source="IdentiCurse")
@@ -123,61 +190,9 @@ class IdentiCurse(object):
         # Why doesn't textpad have a clear method!?
         self.entry_window.clear()
         self.text_entry = textpad.Textbox(self.entry_window)
-        self.update_current()
+        self.update_tabs()
+        self.display_current_tab()
         
-    def update_current(self):
-        if self.current_timeline == "home":
-            self.timelines["home"] = self.conn.statuses_home_timeline(count=25, page=0)
-        elif self.current_timeline == "mentions":
-            self.timelines["mentions"] = self.conn.statuses_mentions(count=25, page=0)
-        elif self.current_timeline == "direct":
-            self.timelines["direct"] = self.conn.direct_messages(count=25, page=0)
-        elif self.current_timeline == "public":
-            self.timelines["public"] = self.conn.statuses_public_timeline()
-
-    def update_timelines(self):
-        self.timelines["home"] = self.conn.statuses_home_timeline(count=25, page=0)
-        self.timelines["mentions"] = self.conn.statuses_mentions(count=25, page=0)
-        self.timelines["direct"] = self.conn.direct_messages(count=25, page=0)
-        self.timelines["public"] = self.conn.statuses_public_timeline()
-
-    def display_current_timeline(self):
-        self.notice_window.erase()
-        tl = self.timelines[self.current_timeline]
-
-        y = 0
-        c = 1
-
-        maxc = self.notice_window.getmaxyx()[0] / 3
-        maxx = self.notice_window.getmaxyx()[1]
-
-        for n in tl:
-            if self.current_timeline == "direct":
-                user = unicode(n["sender"]["screen_name"])
-                source_msg = "" # source parameter cannot be retrieved from a direct, wtf?
-            else:
-                user = unicode(n["user"]["screen_name"])
-                raw_source_msg = "from %s" % (n["source"])
-                source_msg = self.html_regex.sub("", raw_source_msg)    # strip out the link tag that identi.ca adds to some clients' source info
-            
-            self.notice_window.addstr(y,0, str(c))
-            self.notice_window.addstr(y,3, user)
-            self.notice_window.addstr(y,maxx - (len(source_msg) + 2), source_msg)  # right margin of 2 to match the left indentation
-            y += 1
-            text = n["text"]
-
-            try:
-                self.notice_window.addstr(y,4, text.encode("utf-8"))
-            except curses.error:
-                self.notice_window.addstr(y,4, str("Caution: Terminal too shit to display this notice."))
-            
-            y += 2
-            c += 1
-
-            if c == maxc:
-                break
-                
- 
     def quit(self):
         curses.endwin()
         sys.exit()
