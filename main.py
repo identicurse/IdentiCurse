6f0658ff6584d5521b21762ea2efef52d78a9aa8
Psychedelic Squid <psquid@psquid.net>
2010-11-05 03:16:00 +0000

    Slogans!

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 from identicurse import IdentiCurse
+import random
+slogans = [
+            "100% hippie-approved",
+            "powered by hatred",
+            "we don't get OAuth either",
+            "don't drink and dent",
+          ]
 
 # Innit.
-print "Welcome to IdentiCurse"
+print "Welcome to IdentiCurse - %s." % (random.choice(slogans))
 IdentiCurse()
