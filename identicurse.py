46562fe4c3908385546899c03a0d3a100533269b
Psychedelic Squid <psquid@psychedelicsquid.net>
2010-11-11 15:41:20 +0000

    Enter support for multiline entry boxes (Python 2.5 compatible).

--- identicurse.py
+++ identicurse.py
@@ -6,7 +6,7 @@ try:
 except ImportError:
     import simplejson as json
 from threading import Timer
-from curses import textpad
+from textbox import Textbox
 import urllib2
 
 from statusnet import StatusNet
@@ -56,10 +56,7 @@ class IdentiCurse(object):
             entry_lines = (self.conn.length_limit / x) + 1
 
         self.entry_window = self.main_window.subwin(entry_lines, x-10, 4, 5)
-        try:
-            self.text_entry = textpad.Textbox(self.entry_window, insert_mode=True)
-        except TypeError:  # python 2.5 didn't support insert_mode
-            self.text_entry = textpad.Textbox(self.entry_window)
+        self.text_entry = Textbox(self.entry_window, insert_mode=True)
         self.text_entry.stripspaces = 1
 
         self.notice_window = self.main_window.subwin(y-7, x-4, 5 + entry_lines, 5)
@@ -522,10 +519,7 @@ class IdentiCurse(object):
 
 
         self.entry_window.clear()
-        try:
-            self.text_entry = textpad.Textbox(self.entry_window, insert_mode=True)
-        except TypeError:  # python 2.5 didn't support insert_mode
-            self.text_entry = textpad.Textbox(self.entry_window)
+        self.text_entry = Textbox(self.entry_window, insert_mode=True)
         self.text_entry.stripspaces = 1
         self.insert_mode = False
         self.update_tabs()
