dd8273d59b513eb96df67a720c996a5857e13ce0
Psychedelic Squid <psquid@psquid.net>
2011-01-01 00:01:55 +0000

    Happy new year!

--- src/identicurse/tabbage.py
+++ src/identicurse/tabbage.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010 Reality <tinmachin3@gmail.com> and Psychedelic Squid <psquid@psquid.net>
+# Copyright (C) 2010-2011 Reality <tinmachin3@gmail.com> and Psychedelic Squid <psquid@psquid.net>
 # 
 # This program is free software: you can redistribute it and/or modify 
 # it under the terms of the GNU General Public License as published by 
