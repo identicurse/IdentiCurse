0af2b87fb8525d8d8023b40a3eae8ac5431909e9
Psychedelic Squid <psquid@psquid.net>
2011-01-07 00:14:02 +0000

    Begin development on 0.7.

--- src/identicurse/__init__.py
+++ src/identicurse/__init__.py
@@ -85,7 +85,7 @@ def main():
         slogans = user_slogans
     except:
         slogans = preset_slogans
-    print "Welcome to IdentiCurse 0.6-dev - %s" % (random.choice(slogans))
+    print "Welcome to IdentiCurse 0.7-dev - %s" % (random.choice(slogans))
     IdentiCurse(additional_config)
 
 def colour_check():
