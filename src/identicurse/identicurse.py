721860631fbc83fadbe3e464be804b499b9a34f4
Psychedelic Squid <psquid@psychedelicsquid.net>
2011-01-06 22:32:26 -0500

    Well, _that_ was badly done.

--- src/identicurse/identicurse.py
+++ src/identicurse/identicurse.py
@@ -159,7 +159,8 @@ class IdentiCurse(object):
         # Set some defaults for configs that we will always need to use, but that are optional
         if not "enable_colours" in self.config:
             self.config["enable_colours"] = True
-        else:
+
+        if self.config["enable_colours"]:
             default_colour_scheme = {
                 "timelines": ("none", "none"),
                 "statusbar": ("black", "white"),
