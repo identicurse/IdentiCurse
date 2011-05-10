f9d6cbd66e7804ec1ab16d6448b0341c7f752d88
Psychedelic Squid <psquid@psquid.net>
2011-05-10 14:10:44 +0100

    Correctly load simplejson when needed in config.py. Other modules already did this, dunno why it was missed.

--- src/identicurse/config.py
+++ src/identicurse/config.py
@@ -15,7 +15,10 @@
 # You should have received a copy of the GNU General Public License 
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-import json
+try:
+    import json
+except ImportError:
+    import simplejson as json
 
 class Config(dict):
     def __init__(self):
