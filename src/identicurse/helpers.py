2ae740f310b3bd1cd68d77254f048adcbc733518
Psychedelic Squid <psquid@psquid.net>
2012-09-01 22:33:02 +0100

    resetlocale() craps out on Windows, explanatory comments instead

--- src/identicurse/helpers.py
+++ src/identicurse/helpers.py
@@ -36,7 +36,7 @@ def normalise_datetime(timestring):
             break
         except ValueError:  # something else changed the locale, and Python threw a hissy fit
             pass
-    locale.setlocale(locale.LC_TIME, '') # other half of the hacky fix
+    locale.setlocale(locale.LC_TIME, '') # other half of the hacky fix - resets locale
     return normalised_datetime
 
 def time_since(datetime_then):
