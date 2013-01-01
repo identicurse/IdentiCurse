50ae4714aeac30466b8947bf9de8f429d8e9ab5f
Psychedelic Squid <psquid@psquid.net>
2013-01-01 06:24:11 +0000

    http://www.youtube.com/watch?v=Fw1XJbV2HK0

--- src/identicurse/identicurse.py
+++ src/identicurse/identicurse.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 #
-# Copyright (C) 2010-2012 Reality <tinmachin3@gmail.com> and
+# Copyright (C) 2010-2013 Reality <tinmachin3@gmail.com> and
 #                         Psychedelic Squid <psquid@psquid.net>
 #
 # This program is free software: you can redistribute it and/or modify
